a5da9889a67f6b3a1ba9ec8da183bdc5e7bf99bd
Ysquid <ahk648@gmail.com>
2023-01-27 12:48:21 -0600

    added a root /api route

--- functions/routes/todo.js
+++ functions/routes/todo.js
@@ -4,6 +4,10 @@ const cors = require('cors')
 const router = express.Router();
 const { create, read, removeTodo } = require('../controller');
 
+router.get('/', (req, res) => {
+    res.send('todos route with no database connection')
+})
+
 router.get('/todos', read);
 
 router.post('/todo/create', cors(), create);
