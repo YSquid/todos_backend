f7c01ac97351df298cd239e2eeaa842416382d2e
Ysquid <ahk648@gmail.com>
2023-01-28 10:21:32 -0600

    changed host to 0.0.0.0

--- functions/model/database.js
+++ functions/model/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  host: '0.0.0.0',
   database: process.env.DB_DATABASE,
   password: process.env.DB_PW,
   port: process.env.DB_PORT,
