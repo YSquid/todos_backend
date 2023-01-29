641283cd6de0e8e908dac0ef2af6149506166379
Ysquid <ahk648@gmail.com>
2023-01-28 18:37:13 -0600

    0

--- functions/model/database.js
+++ functions/model/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   user: 'postgres',
-  host: '172.16.1.75',
+  host: '0.0.0.0',
   database: 'todos',
   password: 'postgres',
   port: '5432',
