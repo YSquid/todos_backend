34ec1c48ba8198b7ce07de9206b016c418f17f03
Ysquid <ahk648@gmail.com>
2023-01-30 11:15:15 -0600

    refactored url and secret, confirmed local

--- functions/model/database.js
+++ functions/model/database.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SECRET);
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseSecret = process.env.SUPABASE_SECRET;
+
+const supabase = createClient(supabaseUrl, supabaseSecret);
 
 module.exports = supabase;
